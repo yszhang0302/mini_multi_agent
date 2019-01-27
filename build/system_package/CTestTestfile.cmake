1f25a1e53c97cf32e8f54e5c9075dd1170aea8fe
ys_zhang <276690503@qq.com>
2019-01-27 08:17:28 -0800

    setup package

---
+++ build/system_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ys/mini_multi_agent/src/system_package
+# Build directory: /home/ys/mini_multi_agent/build/system_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
