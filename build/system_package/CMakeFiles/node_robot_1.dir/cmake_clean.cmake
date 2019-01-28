6ac5cd7d66f5b60d59bbda5f17e75f3ddf83c6d7
ys_zhang <276690503@qq.com>
2019-01-28 02:11:21 -0800

    node_robot_1 and node_robot_2 source code

---
+++ build/system_package/CMakeFiles/node_robot_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_robot_1.dir/src/node_robot_1.cpp.o"
+  "/home/ys/mini_multi_agent/devel/lib/system_package/node_robot_1.pdb"
+  "/home/ys/mini_multi_agent/devel/lib/system_package/node_robot_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_robot_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/system_package/CMakeFiles/node_robot_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_robot_2.dir/src/node_robot_2.cpp.o"
+  "/home/ys/mini_multi_agent/devel/lib/system_package/node_robot_2.pdb"
+  "/home/ys/mini_multi_agent/devel/lib/system_package/node_robot_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_robot_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
